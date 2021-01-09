5cbd92962ff390d33e74d032567b5ed3aaf54708
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2021-01-09 03:21:17 +0300

    Applying changes for PB's recent requirements from the SDK

--- Source/DiscordGameSDK/Private/DiscordHelper.cpp
+++ Source/DiscordGameSDK/Private/DiscordHelper.cpp
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "DiscordHelper.h"
-
 #include "DiscordLibrary.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogDiscord, All, All)
@@ -15,33 +14,85 @@ static discord::Core* Core = nullptr;
 bool UDiscordHelper::Initialize(int64 ClientID, bool bDiscordRequired)
 {
 	// https://discordapp.com/developers/docs/game-sdk/sdk-starter-guide#code-primer-unreal-engine-4-cpp
-	discord::Result result = discord::Core::Create(ClientID, bDiscordRequired ? DiscordCreateFlags_Default : DiscordCreateFlags_NoRequireDiscord, &Core);
-	if (result != discord::Result::Ok || Core == nullptr)
+	const discord::Result Result = discord::Core::Create(ClientID, bDiscordRequired ? DiscordCreateFlags_Default : DiscordCreateFlags_NoRequireDiscord, &Core);
+	if (Result != discord::Result::Ok || Core == nullptr)
 	{
 		UE_LOG(LogDiscord, Error, TEXT("Failed to create Discord Core"));
 		return false;
 	}
-	
+
+	discord::Event<> OnUserConnectedEvent;
+	auto UserConnectionHandler = [&]()
+	{
+		if (Core)
+		{
+			discord::User CurrentUser;
+			const discord::Result UserResult = Core->UserManager().GetCurrentUser(&CurrentUser);
+			if (UserResult == discord::Result::Ok)
+			{
+				OnUserConnected.Broadcast(CurrentUser.GetUsername(), CurrentUser.GetId(), CurrentUser.GetDiscriminator());
+			}
+		}
+	};
+
+	OnUserConnectedEvent.Connect(UserConnectionHandler);
+	Core->UserManager().OnCurrentUserUpdate = OnUserConnectedEvent;
+
 	return true;
 }
 
-bool UDiscordHelper::RunCallbacks()
+bool UDiscordHelper::RunCallbacks() const
 {
 	if (Core)
+	{
 		return Core->RunCallbacks() == discord::Result::Ok;
-
+	}
 	return false;
 }
 
-bool UDiscordHelper::UpdatePlayActivity(const FString& State, const FString& Details)
+int64 UDiscordHelper::GetConnectedUserID()
+{
+	if (Core)
+	{
+		discord::User CurrentUser;
+		const discord::Result Result = Core->UserManager().GetCurrentUser(&CurrentUser);
+		if (Result == discord::Result::Ok)
+		{
+			return CurrentUser.GetId();
+		}
+	}
+	return 0;
+}
+
+FString UDiscordHelper::GetConnectedUserName()
+{
+	if (Core)
+	{
+		discord::User CurrentUser;
+		const discord::Result Result = Core->UserManager().GetCurrentUser(&CurrentUser);
+		if (Result == discord::Result::Ok)
+		{
+			return CurrentUser.GetUsername();
+		}
+	}
+	return "Unknown Discord User";
+}
+
+bool UDiscordHelper::UpdatePlayActivity(const FString& Details, const FString& State, int64 Timestamp)
 {
 	if (!Core)
+	{
 		return false;
+	}
 
 	discord::Activity activity{};
 	activity.SetType(discord::ActivityType::Playing);
 	activity.SetState(TCHAR_TO_ANSI(*State));
 	activity.SetDetails(TCHAR_TO_ANSI(*Details));
+	activity.GetAssets().SetLargeImage("pretty-cover");
+	activity.GetAssets().SetSmallImage("logo-sq-b");
+	discord::ActivityTimestamps& Timestamps = activity.GetTimestamps();
+	Timestamps.SetStart(Timestamp);
 	Core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
 	{
 		if (result != discord::Result::Ok)
@@ -52,14 +103,31 @@ bool UDiscordHelper::UpdatePlayActivity(const FString& State, const FString& Det
 
 		UE_LOG(LogDiscord, Log, TEXT("Discord Activity Set"));
 	});
-
 	return true;
+}
+
+void UDiscordHelper::ClearPlayActivity()
+{
+	if (Core)
+	{
+		Core->ActivityManager().ClearActivity([](discord::Result){});
+	}
+}
+
+void UDiscordHelper::ValidateOrExit(std::function<void(discord::Result)> Callback)
+{
+	if (Core)
+	{
+		Core->ApplicationManager().ValidateOrExit(Callback);
+	}
 }
 
 bool UDiscordHelper::RegisterSteam(uint32 SteamAppID)
 {
 	if (!Core)
+	{
 		return false;
+	}
 
 	return Core->ActivityManager().RegisterSteam(SteamAppID) == discord::Result::Ok;
 }
--- Source/DiscordGameSDK/Public/DiscordHelper.h
+++ Source/DiscordGameSDK/Public/DiscordHelper.h
@@ -3,10 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "types.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-#include "UObject/CoreOnline.h"
-
-
 #include "DiscordHelper.generated.h"
 
 UCLASS()
@@ -14,14 +13,22 @@ class DISCORDGAMESDK_API UDiscordHelper : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 public:
-	static bool Initialize(int64 ClientID, bool bDiscordRequired = false);
-	static bool RunCallbacks();
+	bool Initialize(int64 ClientID, bool bDiscordRequired = false);
+	bool RunCallbacks() const;
 
 	//
 	// Activities
 	// https://discordapp.com/developers/docs/game-sdk/activities
 	//
-	
-	static bool UpdatePlayActivity(const FString& State, const FString& Details);
+
+	static int64 GetConnectedUserID();
+	static FString GetConnectedUserName();
+	static bool UpdatePlayActivity(const FString& Details, const FString& State, int64 Timestamp);
+	static void ClearPlayActivity();
+	static void ValidateOrExit(std::function<void(discord::Result)> Callback);
 	static bool RegisterSteam(uint32 SteamAppID);
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnUserConnectedSignature, const FString&, UserName, const int64, UserId, const FString&, UserDiscriminator);
+	UPROPERTY(BlueprintAssignable)
+	FOnUserConnectedSignature OnUserConnected;
 };
